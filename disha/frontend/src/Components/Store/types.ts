f52ba4a478fd229aad87297c9694775535e99362
Pratyush Pilli <pratyush.pilli@cimcondigital.com>
2026-01-16 11:03:23 +0530

    Remove disha folder from repository and add to gitignore

--- disha/frontend/src/Components/Store/types.ts
+++
@@ -1,9 +0,0 @@
-export interface RequiredItem {
-  item_no: string;
-  description: string;
-  make?: string;
-  material_group?: string;
-  hsn_code?: string;
-  rate?: number;
-  uom?: string;
-} 
--- disha/migrations/0001_add_rejected_by_column.sql
+++
@@ -1,2 +0,0 @@
--- Add rejected_by column to factory_itemrequest table
-ALTER TABLE factory_itemrequest ADD COLUMN rejected_by VARCHAR(100);
